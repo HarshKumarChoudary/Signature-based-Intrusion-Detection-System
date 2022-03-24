8e04d64880c059f6969673e4094aab36a59fa3c6
HarshKumarChoudary <harshkumarchoudhary144@gmail.com>
2022-03-24 07:36:28 +0530

    part of URL and Signature Detection is done

--- app.py
+++ app.py
@@ -31,15 +31,22 @@ def solution():
         cols = data_test.columns.tolist()
         cols.sort()
         data_test = data_test[cols]
-        print(data_test)
+        # print(data_test)
         data_test = scalerobj.scale(data_test)
         # localhost_save_option = tf.saved_model.SaveOptions(experimental_io_device="/job:localhost")
 
         test_data = pd.DataFrame(data_test)
-        print(test_data)
+        # print(test_data)
         # pickled_model = pickle.load(open('model.pkl', 'rb'))
         # model2 = tf.keras.models.load_model(model_dir, options=localhost_save_option)
         model2 = load_model("model.h5")
         y = model2.predict(test_data)
-        print(y)
-    return render_template("response.html")
+        # print(y)
+        ans = {
+            'benign':y[0][0],
+            'Defacement':y[0][1],
+            'Malicious':y[0][2],
+            'Phishing':y[0][3],
+            'Spam':y[0][4]
+        }
+    return render_template("response.html",value = ans)
